49650505dd36ceb7d91b3268206e9906fc3d5b2a
lauMv <42174201+lauMv@users.noreply.github.com>
2023-01-17 17:00:36 -0400

    set main

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+"""Write a short Python script, that takes two numbers values, where one of them is
+the hours worked by employee and the other is the rate, the equation to calculate
+the payment is (hours * rate), but if the hours is more than 40 the equation is
+(hours * rate + (hours - 40) * rate * 0.5). So we need a script to do this automatically."""
+
+
