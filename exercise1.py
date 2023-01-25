bcc189cfe9a6ea4c8431da81d01c51b15b41fd08
lauMv <42174201+lauMv@users.noreply.github.com>
2023-01-25 16:42:27 -0400

    find a specific word

---
+++ exercise1.py
@@ -0,0 +1,26 @@
+import optparse
+
+
+def get_text():
+    file_name = input('Enter file Name: ')
+    file = open(file_name, 'r')
+    counts = dict()
+    big_count = 0
+    big_word = ''
+    with open("employees.csv") as file:
+        for line in file:
+            words = line.split()
+            for word in words:
+                counts[word] = counts.get(word, 0) + 1
+    return counts
+
+
+def get_num_word(counts):
+    word = input("Word to search: ")
+    print(word + " = " + str(counts[str(word)]))
+
+
+if __name__ == '__main__':
+    words = get_text()
+    print (words)
+    get_num_word(words)
